994d3e3e220b99e0a4f6be32aff53f4d9c4d1e86
Fabricio Navarro <Navarro.fa998@gmail.com>
2022-06-04 19:42:31 -0300

    primera parte

---
+++ EjercicioTema4.java
@@ -0,0 +1,13 @@
+public class EjerciciosTema4 {
+    public static void main(String[] args) {
+	// primera parte
+	int numeroIf = 3;
+	if (numeroIf > 0) {
+	    System.out.println("Es positívo");
+	} else if (numeroIf < 0) {
+	    System.out.println("Es negativo");
+	} else {
+	    System.out.println("Es nulo (0)");
+	}
+    }
+}
